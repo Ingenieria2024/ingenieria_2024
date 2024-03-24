4d997a4165a3114360c481b4882b560dbb2a71f4
AlarMay87 <@>
2024-03-23 23:50:21 -0300

    toque a pirulo

--- pirulo.py
+++ pirulo.py
@@ -1,8 +1,5 @@
 print ("Hola, este es el primer archivo del proyecto")
 agregado = input("Ingrese su nombre: ")
 <<<<<<< HEAD
-print ("sararara")
-
-=======
+print ("sarara")
 print ("bienvenido")
->>>>>>> e1872b77c3ae4b1d5cd51efc5b0d894c520f7baa
