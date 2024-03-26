3379df6be63a1ebcda2d64baa099af327f4fd76b
eric-666-r <eric_ramos999@hotmail.com>
2024-03-26 11:55:21 -0300

    mas lineas agregadas a pirulo.py

--- pirulo.py
+++ pirulo.py
@@ -3,4 +3,5 @@ agregado = input("Ingrese su nombre: ")
 print ("sarasa")
 print ("bienvenido")
 print ("seguimos practicando y quemando a los pirulos")
-
+print ("un dia mas practicando con los pirulos")
+print ("que llegue semana santa")
