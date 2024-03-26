86ff8fd0432d02a46105360d80a43d2eb26d6b3b
Lulilugalvan <lulilugalvan@gmail.com>
2024-03-26 10:30:54 -0300

    nueva modificación

--- pirulo.py
+++ pirulo.py
@@ -2,4 +2,5 @@ print ("Hola, este es el primer archivo del proyecto")
 agregado = input("Ingrese su nombre: ")
 print ("sarasa")
 print ("bienvenido")
+print ("seguimos practicando y quemando a los pirulos")
 
