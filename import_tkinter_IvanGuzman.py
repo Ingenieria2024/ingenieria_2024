d5956e078a9034842637cdaddc9d173fc472ff0c
Guzmán Iván <guzmanivanadriel1995@gmail.com>
2024-04-09 01:07:28 -0300

    App de la UNPAZ

--- import_tkinter_IvanGuzman.py
+++ import_tkinter_IvanGuzman.py
@@ -1,6 +1,8 @@
+# Tkinter
 import tkinter as tk
 from tkinter import ttk
 
+# Funciones
 def biblioteca ():
     print ("Usted ha ingresado a la biblioteca digital de la UNPAZ.")
     
@@ -9,14 +11,17 @@ def geolocalizacion ():
 
 def plan_de_estudio ():
     print ("Usted ha ingresado al plan de estudio de la carrera de TUIAS.")
-    
+
+# Root/Ventana
 root = tk.Tk ()
 root.geometry ("400x250")
 root.title ("Universidad Nacional de José Clemente Paz - APP")
 
+# Etiqueta
 etiqueta = ttk.Label (root, text = "Bienvenidos/as al portal de la Universidad Nacional de José C. Paz", padding = (27, 27), background = "light blue")
 etiqueta.pack ()
 
+# Botones
 boton_biblioteca = ttk.Button (root, text = "Biblioteca", padding = (29, 10), command = biblioteca)
 boton_biblioteca.pack ()
 
@@ -26,4 +31,5 @@ boton_geolocalizacion.pack ()
 boton_plan_de_estudio = ttk.Button (root,text = "Plan de estudio", padding = (20, 10), command = plan_de_estudio)
 boton_plan_de_estudio.pack ()
 
+# Loop eterno
 root.mainloop ()
