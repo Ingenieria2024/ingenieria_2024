bdaaad6a1782ccbfa108e57a2a426620e9708e4b
Bianca0bregon <biancalucia.oc@gmail.com>
2024-04-24 15:26:17 -0300

    Edite el archivo

--- pirulo.py
+++ pirulo.py
@@ -7,6 +7,8 @@ print ("seguimos practicando y quemando a los pirulos")
 print ("un dia mas practicando con los pirulos")
 print ("que llegue semana santa")
 print ("me estoy volviendo loca")
+print("hola, estoy practicando para el parcial")
+print("suerte a todes pirules")
 
 print ("hola")
 print ("buenas")
